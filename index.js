d231139b9c97ffa6eb98d1f1b40b54c488cb0c6e
Holger H <holgheiss@gmail.com>
2018-11-06 22:10:19 +0100

    Put defineProperty() in try/catch. Rescues when trying to define properties that are read-only.

--- index.js
+++ index.js
@@ -1,7 +1,12 @@
 'use strict';
 module.exports = (to, from) => {
 	for (const prop of Reflect.ownKeys(from)) {
-		Object.defineProperty(to, prop, Object.getOwnPropertyDescriptor(from, prop));
+		try{
+			Object.defineProperty(to, prop, Object.getOwnPropertyDescriptor(from, prop));
+		}
+		catch(error) {
+			console.log("mimic-fn could not define property.", error)
+		}
 	}
 
 	return to;
